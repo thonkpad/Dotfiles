15f5d3ea8d707e4cafd4cf935c63f7ce96b1f26c
thonkpad <ejanred2@protonmail.com>
2020-06-23 23:45:27 +0800

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -1,28 +1,28 @@
-import XMonad
-import System.Exit
-import System.IO
-import Graphics.X11.ExtraTypes.XF86
-import Control.Monad
+import           XMonad
+import           System.Exit
+import           System.IO
+import           Graphics.X11.ExtraTypes.XF86
+import           Control.Monad
 
-import qualified XMonad.StackSet as W
+import qualified XMonad.StackSet               as W
 
-import Data.Maybe
-import Data.List
-import Data.Monoid
+import           Data.Maybe
+import           Data.List
+import           Data.Monoid
 
-import XMonad.Util.SpawnOnce (spawnOnce)
-import XMonad.Util.Run
-import XMonad.Util.EZConfig (additionalKeys)
-import XMonad.Util.Ungrab
+import           XMonad.Util.SpawnOnce          ( spawnOnce )
+import           XMonad.Util.Run
+import           XMonad.Util.EZConfig           ( additionalKeys )
+import           XMonad.Util.Ungrab
 
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageHelpers
+import           XMonad.Hooks.EwmhDesktops
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Hooks.ManageHelpers
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.ManageHelpers
 
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Fullscreen
+import           XMonad.Layout.NoBorders
+import           XMonad.Layout.Fullscreen
 
 
 {-----------------------------------------------------}
@@ -32,59 +32,63 @@ main = do
 --  xmproc <- spawnPipe "xmobar"
   xmonad =<< xmobar defaults
 
+-- Status Bar
+
 -- Settings and keybindings
 defaults = ewmh $ docks def {
-    terminal        = "kitty" -- kitty as default terminal
-  , modMask         = mod4Mask -- Super key as Mod key
-  , startupHook     = myStartupHook <+> setFullscreenSupported
-  , layoutHook      = myLayoutHook
-  , manageHook      = manageDocks <+> fullscreenManageHook <+> myManageHook
-  , logHook         = dynamicLog
-  , handleEventHook = handleEventHook def <+> XMonad.Hooks.EwmhDesktops.fullscreenEventHook
-               }
-           `additionalKeys` {- My Keybindings -}
-           [
-           -- default app launcher
-             ((mod4Mask, xK_p ),               spawn "rofi -show drun -theme gruvbox-dark-hard")
-           -- Volume Control
-           , ((0, xF86XK_AudioMute),           spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
-           , ((0, xF86XK_AudioLowerVolume),    spawn "pactl set-sink-volume @DEFAULT_SINK@ -5%")
-           , ((0, xF86XK_AudioRaiseVolume),    spawn "pactl set-sink-volume @DEFAULT_SINK@ +5%")
-           -- Brightness Control
-           , ((0, xF86XK_MonBrightnessUp),     spawn "xbacklight +5")
-           , ((0, xF86XK_MonBrightnessDown),   spawn "xbacklight -5")
-           -- Screenshot
-           , ((0,         xK_Print),           spawn "scrot -q 90 /home/thonkpad/Pictures/scrot-%F-%T.png")
-           , ((shiftMask, xK_Print), unGrab >> spawn "scrot -q 90 -s /home/thonkpad/Pictures/scrot-%F-%T.png")
-           ]
+     terminal        = "kitty" -- kitty as default terminal
+   , modMask         = mod4Mask -- Super key as Mod key
+   , startupHook = myStartupHook <+> setFullscreenSupported
+   , layoutHook      = myLayoutHook
+   , manageHook = manageDocks <+> fullscreenManageHook <+> myManageHook
+   , logHook         = dynamicLog
+   , handleEventHook = handleEventHook def <+> XMonad.Hooks.EwmhDesktops.fullscreenEventHook
+                } `additionalKeys` {- My Keybindings -}
+                     [
+                     -- default app launcher
+                       ( (mod4Mask, xK_p), spawn "rofi -show drun -theme gruvbox-dark-hard")
+                     -- Volume Control
+                     , ( (0, xF86XK_AudioMute), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
+                     , ( (0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ -5%")
+                     , ( (0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ +5%")
+                     -- Brightness Control
+                     , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +5")
+                     , ( (0, xF86XK_MonBrightnessDown), spawn "xbacklight -5")
+                     -- Screenshot
+                     , ( (0, xK_Print), spawn "scrot -q 90 /home/thonkpad/Pictures/scrot-%F-%T.png")
+                     , ( (shiftMask, xK_Print), unGrab >> spawn "scrot -q 90 -s /home/thonkpad/Pictures/scrot-%F-%T.png")
+                     ]
 
 -- Programs to start upon logging in to Xmonad
 myStartupHook = do
   spawnOnce "nitrogen --restore &"
   spawnOnce "picom &"
---  spawnOnce "xfce4-power-manager &"
   spawnOnce "dunst &"
   spawnOnce "xscreensaver &"
+  spawnOnce "stalonetray &"
+  spawnOnce "nm-tray &"
+  spawnOnce "udiskie -ant &"
 
 -- Tiling layouts for Xmonad
 myLayoutHook = smartBorders $ avoidStruts (tiled ||| Mirror tiled ||| Full)
-  where
+ where
     -- default tiling algorithm partitions the screen into two panes
-    tiled   = Tall nmaster delta ratio
-    -- The default number of windows in the master pane
-    nmaster = 1
-    -- Default proportion of screen occupied by master pane
-    ratio   = 1/2
-    -- Percent of screen to increment by when resizing panes
-    delta   = 3/100
+  tiled   = Tall nmaster delta ratio
+  -- The default number of windows in the master pane
+  nmaster = 1
+  -- Default proportion of screen occupied by master pane
+  ratio   = 1 / 2
+  -- Percent of screen to increment by when resizing panes
+  delta   = 3 / 100
 
 -- What to do with certain programs upon executing
 -- `doFloat` automatically sends the program to the floating layer
 myManageHook = composeAll
-  [ className =? "mpv"                                 --> doFloat
+  [ className =? "mpv" --> doFloat
   , (className =? "Firefox" <&&> resource =? "Dialog") --> doFloat
-  , className =? "nm-tray"                             --> doFloat
-  , isFullscreen                                       --> doFullFloat
+  , className =? "nm-tray" --> doFloat
+  , className =? "stalonetray" --> doIgnore
+  , isFullscreen --> doFullFloat
   ]
 
 
@@ -92,14 +96,19 @@ myManageHook = composeAll
 -- THIS IS FOR FULLSCREEN SUPPORT
 -- source: https://www.reddit.com/r/xmonad/comments/gc4b9i/what_is_the_best_way_to_make_xmonad_respect_true/fpbnsv9/
 setFullscreenSupported :: X ()
-setFullscreenSupported = addSupported ["_NET_WM_STATE", "_NET_WM_STATE_FULLSCREEN"]
+setFullscreenSupported =
+  addSupported ["_NET_WM_STATE", "_NET_WM_STATE_FULLSCREEN"]
 
 addSupported :: [String] -> X ()
 addSupported props = withDisplay $ \dpy -> do
-    r <- asks theRoot
-    a <- getAtom "_NET_SUPPORTED"
-    newSupportedList <- mapM (fmap fromIntegral . getAtom) props
-    io $ do
-      supportedList <- fmap (join . maybeToList) $ getWindowProperty32 dpy a r
-      changeProperty32 dpy r a aTOM propModeReplace (nub $ newSupportedList ++ supportedList)
-
+  r                <- asks theRoot
+  a                <- getAtom "_NET_SUPPORTED"
+  newSupportedList <- mapM (fmap fromIntegral . getAtom) props
+  io $ do
+    supportedList <- fmap (join . maybeToList) $ getWindowProperty32 dpy a r
+    changeProperty32 dpy
+                     r
+                     a
+                     aTOM
+                     propModeReplace
+                     (nub $ newSupportedList ++ supportedList)
